627515e2043760b919ccae03d20cba6a3e73e75f
lefitzpatrick <ljob@umich.edu>
2025-05-19 14:32:03 -0400

    edits to saved trained models

--- src/data_processing.py
+++ src/data_processing.py
@@ -231,10 +231,10 @@ def process_grib_files(download_dir, database, table, cfs_run, mask_lat, mask_lo
         try:
             flx_2mabove = cfgrib.open_dataset(flx_file, engine='cfgrib', filter_by_keys={'typeOfLevel': 'heightAboveGround', 'level': 2}, decode_timedelta=False)
             try:
-                mean2t = flx_2mabove['mean2t']
-            except KeyError:
-                print("'mean2t' not found in flux file, trying 'avg_2t'.")
                 mean2t = flx_2mabove['avg_2t']
+            except KeyError:
+                print("'avg_2t' not found in flux file, trying 'mean2t'.")
+                mean2t = flx_2mabove['mean2t']
 
             # Cut the variable to the mask domain
             mean2t_cut = mean2t.sel(
@@ -265,10 +265,10 @@ def process_grib_files(download_dir, database, table, cfs_run, mask_lat, mask_lo
         try:
             flx_surface = cfgrib.open_dataset(flx_file, engine='cfgrib', filter_by_keys={'typeOfLevel': 'surface'}, decode_timedelta=False)
             try:
-                mslhf = flx_surface['mslhf']
-            except KeyError:
-                print("'mslhf' not found in flux file, trying 'avg_slhtf'.")
                 mslhf = flx_surface['avg_slhtf']
+            except KeyError:
+                print("'avg_slhtf' not found in flux file, trying 'mslhf'.")
+                mslhf = flx_surface['mslhf']
 
             # Cut the variable to the mask domain
             mslhf_cut = mslhf.sel(
