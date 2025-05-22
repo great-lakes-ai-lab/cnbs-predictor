b9a1fcc5e733d501f638685290e5233834aa3d4b
DaniJonesOcean <dcjones.work@gmail.com>
2025-05-22 08:00:38 -0400

    Add very basic sphinx documentation

---
+++ docs/source/conf.py
@@ -0,0 +1,32 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys 
+sys.path.insert(0, os.path.abspath('../..'))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'cnbs-predictor'
+copyright = '2025, Lindsay Fitzpatrick, Dani Jones'
+author = 'Lindsay Fitzpatrick, Dani Jones'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']  # napoleon helps with Google-style docstrings
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+#html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
